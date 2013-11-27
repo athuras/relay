b4ae1d565d5016fb4f8b4878fbb56071c449124a
Alexander Huras <alex.huras@gmail.com>
2013-11-27 17:23:26 -0500

    updated for api_key routing (unstable)

--- relay_flask/relay.py
+++ relay_flask/relay.py
@@ -1,20 +1,29 @@
 import os
 
 from flask import Flask
-from flask import redirect, url_for
+from flask import json
+from flask import redirect
+from flask import render_template
+from flask import request, Response
+from flask import session, escape
+from flask import url_for
 
 app = Flask(__name__)
 app.secret_key = os.environ.get('APP_SECRET', None)
+cm_api_key = os.environ.get('CM_API', 'e440fa3faa334156831adb28596d54a0')
 
 @app.route('/')
 def home():
     return 'Hello World'
 
-@app.route('/maps')
-def show_map():
+@app.route('/default_map')
+def default_map():
     return redirect(url_for('static', filename='default_map.html'))
+
+@app.route('/map/<api_key>')
+def show_map(api_key=cm_api_key):
+    return render_template('default_map.html', api_key=api_key)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    #cloudmade_api_key = str(os.environ.get('CM_API', 'e440fa3faa334156831adb28596d54a0'))
     app.run(host='0.0.0.0', port=port, debug=True)
