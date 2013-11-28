dcedf162ee4ee21863718530f0ada6c6e0c40b01
Scott Neil <dscott.neil@gmail.com>
2013-11-28 17:11:00 -0500

    adding major multi and single

--- relay_flask/relay.py
+++ relay_flask/relay.py
@@ -36,7 +36,7 @@ def get_intersections():
         bounds = request.json # dictionary of: minlat, maxlat, minlong, maxlong
         qstr = '''SELECT long, lat, name, type, type_short, int_id FROM 
             intersections WHERE lat>=:minlat AND lat<=:maxlat AND 
-            long>=:minlong AND long<=:maxlong and type_short='MJRSL';'''
+            long>=:minlong AND long<=:maxlong and type_short IN  ('MJRML', 'MJRSL');'''
         intersects = g.db.query('relay_main', qstr, bounds, True)
         return createJSON(intersects)
 
