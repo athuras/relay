39e8d111eb290fbbce9f15207bfeec21900ce981
Myles Tan <mylestan7@gmail.com>
2013-11-28 02:26:16 -0500

    dots now appear.

--- relay_flask/static/default_map.js
+++ relay_flask/static/default_map.js
@@ -18,6 +18,18 @@ var intersections = [
 	}
 ];
 
+// Make a single popup
+var popup = L.popup();
+
+// create map
+var map = L.map('map').setView([43.673513374000002, -79.573719920000002], 14);
+
+// add tile layer
+L.tileLayer('http://{s}.tile.cloudmade.com/e440fa3faa334156831adb28596d54a0/115014/256/{z}/{x}/{y}.png', {
+	maxZoom: 18,
+	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
+}).addTo(map);
+
 // init function
 function init(){
 	//sample bounds
@@ -41,61 +53,112 @@ function getIntersections(b){
 		bounds.maxlat = b.getNorth();
 		bounds.maxlong = b.getEast();
 	}
-	$.ajax({
-		type: "POST",
-		datatype: "JSON",
-		url: "http://localhost:5000/request_intersections",
-		data: JSON.stringify(bounds),
-		async: false,
-		success: function(){
-			console.log(data);
-			// prepData(data);
-		},
-		error: function(){
-			console.log('request for data failed');
-		}
+	$.when(
+		$.ajax({
+			type: "POST",
+			datatype: "JSON",
+			url: "http://localhost:5000/request_intersections",
+			data: JSON.stringify(bounds),
+			async: false
+		})
+	).then( function(data){
+		//passed
+		console.log('passed!');
+		intersections = prepData(data);
+		populateMap();
+	}, function(error){
+		//failed
+		console.log('shit :(');
+		console.log(error);
+		var data = [{"lat": 43.617252641, "long": -79.378593649, "name": ""}, {"lat": 43.615640333, "long": -79.379787026, "name": "Lagoon Rd / Centre Island Pk"}, {"lat": 43.616748867, "long": -79.381405861, "name": "Centre Island Pk"}, {"lat": 43.615644162, "long": -79.383859851, "name": "Centre Island Pk"}, {"lat": 43.615531685, "long": -79.385420298, "name": ""}, {"lat": 43.616634859, "long": -79.3843377, "name": "Centre Island Pk"}, {"lat": 43.616556566, "long": -79.384080588, "name": "Centre Island Pk"}, {"lat": 43.617642375, "long": -79.385550662, "name": "Centre Island Pk"}, {"lat": 43.619474279, "long": -79.385665726, "name": ""}, {"lat": 43.614982991, "long": -79.38971764, "name": "Beach Rd / Lakeshore Ave"}, {"lat": 43.615058259, "long": -79.387234747, "name": ""}, {"lat": 43.613189796, "long": -79.386956572, "name": "Beach Rd / Lakeshore Ave"}, {"lat": 43.612246478, "long": -79.388877878, "name": "Beach Rd"}, {"lat": 43.613139077, "long": -79.382615663, "name": "Lakeshore Ave"}, {"lat": 43.613987775, "long": -79.379351345, "name": "Lakeshore Ave / Centre Island Pk"}, {"lat": 43.617412954, "long": -79.375421729, "name": "The Mall Ter"}, {"lat": 43.615598892, "long": -79.375206659, "name": "Lakeshore Ave / The Mall Ter"}, {"lat": 43.6169786, "long": -79.372422412, "name": "Lakeshore Ave / Avenue Of The Islands"}, {"lat": 43.61640393, "long": -79.3719358, "name": "Avenue Of The Islands"}, {"lat": 43.619214799, "long": -79.374476208, "name": "Avenue Of The Islands"}, {"lat": 43.619214799, "long": -79.374476208, "name": "Avenue Of The Islands"}, {"lat": 43.618969922, "long": -79.37421219, "name": "Avenue Of The Islands"}, {"lat": 43.618969922, "long": -79.37421219, "name": "Avenue Of The Islands"}, {"lat": 43.618529739, "long": -79.373837027, "name": "Cibola Ave / Avenue Of The Islands"}, {"lat": 43.61818324, "long": -79.374574654, "name": "Lagoon Rd / Cibola Ave"}, {"lat": 43.617682354, "long": -79.375640868, "name": "Cibola Ave / The Mall Ter"}, {"lat": 43.617815333, "long": -79.376242282, "name": "The Mall Ter"}, {"lat": 43.617750181, "long": -79.377052122, "name": "Lagoon Rd / The Mall Ter"}, {"lat": 43.61992857, "long": -79.375129707, "name": "Island Park Trl / Avenue Of The Islands"}, {"lat": 43.613224114, "long": -79.38333629, "name": "Lakeshore Ave"}, {"lat": 43.617139201, "long": -79.38028221, "name": "Centre Island Pk"}, {"lat": 43.618014649, "long": -79.385084061, "name": "Centre Island Pk"}, {"lat": 43.616752557, "long": -79.37319924, "name": "Lagoon Rd / Lakeshore Ave / The Mall Ter"}, {"lat": 43.617519421, "long": -79.375987695, "name": "Cibola Ave / The Mall Ter"}, {"lat": 43.617249165, "long": -79.375742401, "name": "The Mall Ter"}, {"lat": 43.619472434, "long": -79.3737402, "name": "Island Park Trl"}];
+		intersections = prepData(data);
+		populateMap(intersections);
 	});
 }
 
-// create map
-var map = L.map('map').setView([43.673513374000002, -79.573719920000002], 14);
+function prepData(data){
+	// Add some bogus data to them!
+	for(i in data){
+		if(data.hasOwnProperty(i)){
+			data[i].id = Math.floor((Math.random()*10000));
 
-// add tile layer
-L.tileLayer('http://{s}.tile.cloudmade.com/e440fa3faa334156831adb28596d54a0/115014/256/{z}/{x}/{y}.png', {
-	maxZoom: 18,
-	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
-}).addTo(map);
+			var stat = Math.random();
+			data[i].online = (stat < 0.05)? false : true;
+			data[i].status = (stat < 0.05)? "MAINTENANCE" : "OK";
 
-// Make a single popup
-var popup = L.popup();
+			data[i].volume = Math.random();
+			data[i].performance = Math.random();
+		}
+	}
+	return data;
+}
 
-for(inter in intersections){
-	if(intersections.hasOwnProperty(inter)){
-		var i = intersections[inter];
+function populateMap(intersections) {
+	for(inter in intersections){
+		if(intersections.hasOwnProperty(inter)){
+			var i = intersections[inter];
 
-		// background marker
-		L.circleMarker([i['x'], i['y']], {
-			stroke: false,
-			fill: true,
-			fillOpacity: 0.2,
-			radius: i['volume']/2,
+			var oColor, oRadius, oOpacity;
+			var iColor, iRadius, iOpacity;
 
-			clickable: false
-		})
-			.addTo(map);
+			oColor = '#fff';
+			oRadius = 20;
+			oOpacity = 0.05;
 
-		// action marker
-		L.circleMarker([i['x'], i['y']], {
-			id: i['id'],
-			stroke: false,
-			fill: true,
-			fillOpacity: 0.9,
-			radius: 10,
+			iColor = '#fff';
+			iRadius = 5;
+			iOpacity = 0.7;
 
-			clickable: true
-		})
-		.addTo(map)
-		.on('click',populatePopupForIntersection);
+			//decide what happens to the point!
+			// if(i.online){
+			// 	oColor = '#fff';
+			// 	oRadius = iRadius + Math.floor(30*i.volume);
+			// 	oOpacity = 0.05;
+
+			// 	if(i.performance > 0.5){
+			// 		iColor = '#OfO';
+			// 	}
+			//  else {
+			// 		iColor = '#f00';
+			// 	}
+			// 	iRadius = 10;
+			// 	iOpacity = 0.7;
+			// } else {
+			// 	oColor = '#000';
+			// 	oRadius = 20;
+			// 	oOpacity = 0.2;
+
+			// 	iColor = '#000';
+			// 	iRadius = 10;
+			// 	iOpacity = 0.9;
+			// }
+
+			// background marker
+			L.circleMarker([i['lat'], i['long']], {
+				stroke: false,
+				fill: true,
+				clickable: false,
+
+				fillOpacity: oOpacity,
+				radius: oRadius,
+				color: oColor
+			})
+				.addTo(map);
+
+			// action marker
+			L.circleMarker([i['lat'], i['long']], {
+				id: i['id'],
+				stroke: false,
+				fill: true,
+				clickable: true,
+
+				fillOpacity: iOpacity,
+				radius: iRadius,
+				color: iColor
+			})
+			.addTo(map)
+			.on('click',populatePopupForIntersection);
+		}
 	}
 }
 
@@ -103,10 +166,10 @@ function populatePopupForIntersection(event){
 	intersection = getIntersectionById(event.target.options.id);
 
 	var content = "";
-	content += "<h1>" + intersection.title + "</h1>";
+	content += "<h1>" + intersection.name + "</h1>";
 	content += "<a onclick='populatePanelForIntersection(" + intersection.id + ")'>more</a>";
 
-	var latlng = L.latLng(intersection.x, intersection.y);
+	var latlng = L.latLng(intersection.lat, intersection.long);
 
 	var popup = L.popup()
 	    .setLatLng(latlng)
@@ -121,8 +184,8 @@ function populatePanelForIntersection(id){
 	var intersection = $.grep(intersections, function(i){ return i.id == id});
 	intersection = intersection[0]; //only one?
 
-	$('#intersection-title').html(intersection['title']);
-	$('#intersection-performance').html(String(intersection['performance']));
+	$('#intersection-title').html(intersection.name);
+	$('#intersection-performance').html(String(intersection.performance));
 }
 
 function getIntersectionById(id){
