9577e576098e9e0d5d5edf1d3ab2d73c64c850b7
Alexander Huras <alex.huras@gmail.com>
2014-03-02 22:09:12 -0500

    better flow, added barrier method to handle constraints

--- lambda_agent/pyCrunch/graph_select.py
+++ lambda_agent/pyCrunch/graph_select.py
@@ -1,65 +1,51 @@
-import numpy as np
-import igraph as ig
 from erlport.erlterms import Atom, List
-from collections import defaultdict
+from scipy.optimize import anneal
+import numpy as np
 
 
-def find_path(start, BTG, B, P, d):
-    '''Find the best path through BTG starting at *start*,
-    * BTG: Behaviour Transition Graph [(v1, v2, tau_{v1,v2})]
-    * B: Behaviour Vector Matrix of shape (n, d).
-    * P: Prediction of shape (d, T/dt).
-    * d: Max Depth (solution is of length d + 1).
-    * start: initial behaviour.
+def optimize_path(paths, behaviours, btg, prediction, dt=1.):
+    '''Erlang Entry Point to Optimization Module'''
+    B_table = parse_behaviours(behaviours)
+    BTG = parse_edgelist(btg)
+    F = parse_prediction(prediction)
 
-    solves for D = [B_0, t_0, ..., B_d, t_d]
-    s.t. t_i >= T_{i-1} - tau_{i, i-1},
-    where tau_{a, b} = dead (red) time between states a, b.
-    '''
-    Nt, d = len(P), len(P[0])
-    P = np.array(P).reshape((Nt, d))
-    P_cumulative = np.cumsum(P, axis=1)
+    BestPath = best_path(paths, B_table, BTG, F, dt=dt)
+    return BestPath
 
-    BTG # A graph, probably as an edge-list
-    return []
-
-
-def best_path(paths, behaviours, btg, prediction, dt=1., tmax=200.):
+def best_path(paths, Behaviour_Table, BTG, F, dt=1.):
     '''
     Perform the mixed ILP optimization (without queues, or memory), that yields
     the optimal behaviour transition through the BTG.
 
     :paths      -> iterable of path-iterables, path-domain for optimization
                    Each path-iterable contains only behaviour_id.
-    :behaviours -> map of behaviour_id to  behaviour_vec
+    :Behaviour_Table    -> Dict of the form {behaviour_id: <behaviour_vec>}
                    Must contain all behaviours in btg
     :btg        -> Behaviour Transition Graph, nodes are behaviour_ids,
-                   Edges are of the form {v_1, v_2, tau_{1,2}}
-    :prediction -> matrix, of shape (|b_vec|, n), where n is int(T_max/dt)
+                   dictionary of the form {(v_1, v_2): tau_1,2}
+    :F          -> Prediction matrix, of shape (|b_vec|, n),
+                    where n is int(T_max/dt)
+    :dt         -> Prediction time-resolution
     '''
-    from scipy.optimize import anneal  # We work hard, we play hard.
-
-    B_table = parse_behaviours(behaviours)
-    BTG = parse_edgelist(btg)
-    F = parse_prediction(prediction)
-
     # Given a particular path, find the optimal times to transition
     cum_F = np.cumsum(F, axis=1)
 
     Solutions = []
     for path in paths:
-        B = np.vstack(B_table[bid] for bid in path)  # Behaviour Matrix (d,4)
+        B = np.vstack(Behaviour_Table[bid] for bid in path)  # Behaviour Matrix (d,4)
         taus = gen_costs(path, BTG)
-        x = initial_soln(path, t_max)
-        def cost(times): return -1 * obj(times, B, cum_F, taus, dt=dt)
+        x = initial_soln(path, int(F.shape[-1] * dt)
+
+        cost = lambda x:  -1 * obj(x, B, cum_F, taus, dt=dt)  # Set to std form
+        barrier = lambda x: 50 * barrier(x, path, BTG)  # Constraint Programming
+        L = lambda x: cost(x) + barrier(x)  # Combined Function
 
         lower = np.cumsum(np.array(taus))
         upper = tmax - lower
-        sol = anneal(cost, x, upper=upper, lower=lower, maxiter=250)
+
+        sol = anneal(L, x, upper=upper, lower=lower, maxiter=250)
         Solutions.append(sol)
-    (tmin, Jmin, T, feval,
-        iters, accept, status) =  min(Solutions, key=lambda x: x[1])
-    return tmin
+    return min(Solutions, key=lambda x: x[1])
 
 
 #  Parsers      ###############################################################
@@ -69,14 +55,14 @@ def parse_edgelist(edges):
 
 def parse_behaviours(behaviours):
     '''[(bid, <bvec>)] -> {bid: <bvec>}'''
-    return {bid: bvec for bid, bvec in behaviours}
+    return {bid: np.array(tuple(bvec)) for bid, bvec in behaviours}
 
 def parse_prediction(F):
     '''[[float]] -> np.array(...) of same shape'''
     return np.array(F)  # Might not work, will check back later
 
-#  Optimization ###############################################################
 
+#  Optimization ###############################################################
 def initial_soln(path, t_max):
     '''Evenly Distributed, no check for taus'''
     j = t_max / len(path)
@@ -107,3 +93,13 @@ def obj(times, B, cum_F, costs, dt=1.):
     '''Objective Function for Hillclimbing'''
     Z = B * time_costs(cum_F, times, costs, dt=dt)
     return Z.sum()
+
+def barrier(times, path, taus):
+    '''Handles Linear/causality Constraints with respect to transitions'''
+    t = [0] + times
+    S = 0.
+    for i in xrange(len(path) - 1):
+        edge = (path[i], path[i + 1])
+        tau = taus[edge]
+        S += abs(times[i + 1] - times[i] + tau)**2
+    return S
