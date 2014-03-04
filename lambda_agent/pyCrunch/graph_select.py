fabae3eed75490f8accb61329bd5d4e2e73e2f94
Alexander Huras <alex.huras@gmail.com>
2014-03-03 20:54:29 -0500

    working optimization, now uses basinhopping

--- lambda_agent/pyCrunch/graph_select.py
+++ lambda_agent/pyCrunch/graph_select.py
@@ -1,18 +1,30 @@
-from erlport.erlterms import Atom, List
-from scipy.optimize import anneal
+from erlport.erlterms import Atom
+from scipy.optimize import basinhopping
 import numpy as np
 
+class Bounds(object):
+    '''Required for acceptance testing in scipy.optimize.basinhopping'''
+    def __init__(self, xmin=1., xmax=0.):
+        self.xmax = xmax
+        self.xmin = xmin
+    def __call__(self, **kwargs):
+        x = kwargs["x_new"]
+        tmax = bool(np.all(x <= self.xmax))
+        tmin = bool(np.all(x >= self.xmin))
+        return tmax and tmin
 
-def optimize_path(paths, behaviours, btg, prediction, dt=1.):
+
+def optimize_path(paths, B_table, btg, prediction, dt=1.):
     '''Erlang Entry Point to Optimization Module'''
     B_table = parse_behaviours(behaviours)
     BTG = parse_edgelist(btg)
     F = parse_prediction(prediction)
 
     BestPath = best_path(paths, B_table, BTG, F, dt=dt)
-    return BestPath
+    return BestPath.x
 
-def best_path(paths, Behaviour_Table, BTG, F, dt=1.):
+def best_path(paths, Behaviour_Table, BTG, F, dt=1.,
+              maxiter=400):
     '''
     Perform the mixed ILP optimization (without queues, or memory), that yields
     the optimal behaviour transition through the BTG.
@@ -31,21 +43,28 @@ def best_path(paths, Behaviour_Table, BTG, F, dt=1.):
     cum_F = np.cumsum(F, axis=1)
 
     Solutions = []
+    t_max = int((F.shape[-1] - 1) * dt)
     for path in paths:
-        B = np.vstack(Behaviour_Table[bid] for bid in path)  # Behaviour Matrix (d,4)
-        taus = gen_costs(path, BTG)
-        x = initial_soln(path, int(F.shape[-1] * dt)
+        (L, x0, bounds) = opt_params(path, Behaviour_Table,
+                                    BTG, t_max, cum_F, dt=dt)
+        result = basinhopping(L, x0, accept_test=bounds, stepsize=3*dt,
+                                niter=maxiter, T=10.)
+        Solutions.append(result)
 
-        cost = lambda x:  -1 * obj(x, B, cum_F, taus, dt=dt)  # Set to std form
-        barrier = lambda x: 50 * barrier(x, path, BTG)  # Constraint Programming
-        L = lambda x: cost(x) + barrier(x)  # Combined Function
+    return min(((i, s) for i, s in enumerate(Solutions)), key=lambda x: x[1].fun)
 
-        lower = np.cumsum(np.array(taus))
-        upper = tmax - lower
 
-        sol = anneal(L, x, upper=upper, lower=lower, maxiter=250)
-        Solutions.append(sol)
-    return min(Solutions, key=lambda x: x[1])
+def opt_params(path, B_Table, BTG, t_max, cum_F, dt):
+    B = np.vstack(B_Table[bid] for bid in path)  # Behaviour Matrix (d,4)
+    taus = gen_costs(path, BTG)
+    x = initial_soln(path, t_max)
+
+    cost = lambda x:  -1 * obj(x, B, cum_F, taus, dt=dt)  # Set to std form
+    constraints = lambda x: 1000 * barrier(x, path, BTG)  # Constraint Programming
+    L = lambda x: cost(x) + constraints(x)  # Combined Function
+
+    bounds = Bounds(0., (cum_F.shape[-1] - 1) * dt)
+    return L, x, bounds
 
 
 #  Parsers      ###############################################################
@@ -53,9 +72,9 @@ def parse_edgelist(edges):
     '''[(a, b, tau)] -> {(a, b): tau}'''
     return {(a, b): tau for a, b, tau in edges}
 
-def parse_behaviours(behaviours):
+def parse_behaviours(behaviours, dtype=np.float32):
     '''[(bid, <bvec>)] -> {bid: <bvec>}'''
-    return {bid: np.array(tuple(bvec)) for bid, bvec in behaviours}
+    return {bid: np.array(tuple(bvec), dtype=dtype) for bid, bvec in behaviours}
 
 def parse_prediction(F):
     '''[[float]] -> np.array(...) of same shape'''
@@ -66,27 +85,31 @@ def parse_prediction(F):
 def initial_soln(path, t_max):
     '''Evenly Distributed, no check for taus'''
     j = t_max / len(path)
-    return np.array(i + j for i in xrange(len(path)))
+    return np.array([(i + 1) * j for i in xrange(len(path) - 1)])
 
 def gen_costs(path, btg):
     return [btg[(path[i], path[i+1])] for i in xrange(len(path) - 1)]
 
-def range_sum(cum_F, a, b):
-    return cum_F[..., b] - cum_f[..., a]
+def range_sum(cum_F, a, b, penalty=-1000):
+    '''Penalty brutally dominates any out-of-index operation...'''
+    z = cum_F.shape[-1] - 1
+    if (not 0 <= a <= z) or (not 0 <= b <= z):
+        return np.ones(cum_F.shape[0]) * penalty
+    return cum_F[..., b] - cum_F[..., a]
 
 def time_costs(cum_F, times, costs, dt=1.):
     '''Times: [t1, ..., td],
     costs: [t_{b0, b1}, t_{b1, b2}, ...]'''
-    discr_index = lambda x: int(x / dt)
+    discr_index = lambda x: int(x / dt) - 1
     t_steps = map(discr_index, times)
     t_steps = [0] + t_steps
-    t_steps.append(cum_F.shape[-1])  # t_max
+    t_steps.append(cum_F.shape[-1] - 1)  # t_max
 
     c_steps = map(discr_index, costs)
     c_steps = [0] + c_steps
 
     result = np.vstack([range_sum(cum_F, t_steps[i] + c_steps[i], t_steps[i + 1])
-                        for i in xrange(len(costs))])
+                        for i in xrange(len(costs) + 1)])
     return result
 
 def obj(times, B, cum_F, costs, dt=1.):
@@ -94,12 +117,12 @@ def obj(times, B, cum_F, costs, dt=1.):
     Z = B * time_costs(cum_F, times, costs, dt=dt)
     return Z.sum()
 
-def barrier(times, path, taus):
+def barrier(times, path, BTG):
     '''Handles Linear/causality Constraints with respect to transitions'''
-    t = [0] + times
+    t = [0] + list(times)
     S = 0.
     for i in xrange(len(path) - 1):
         edge = (path[i], path[i + 1])
-        tau = taus[edge]
-        S += abs(times[i + 1] - times[i] + tau)**2
+        tau = BTG[edge]
+        S += min(0, (t[i + 1] - t[i] - tau))  # Only accrue if constraint is voilated
     return S
