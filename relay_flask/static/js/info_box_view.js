ea9bceb346e603faeb7e0e1cc45b7ea0fe891bbb
Myles Tan <mylestan7@gmail.com>
2014-03-13 14:23:41 -0400

    added info box view

---
+++ relay_flask/static/js/info_box_view.js
@@ -0,0 +1,77 @@
+var InfoBoxView = Backbone.View.extend({
+	el: $('#info-box'),
+
+	defaults: {
+		'currentModel': null,
+		'currentMarker': null,
+		'showMoreDom': null,
+	},
+
+	events: {
+		'click #more-info-link': 'showMoreClicked',
+		'click' : 'clicked'
+	},
+
+	initialize: function(options){
+		// we keep reference of the map for convenience.
+		this.map = options.map;
+		this.mapPageView = options.mapPageView;
+
+		// info box options use these to style the box!
+		this.boxText = $('<div></div>').loadTemplate('#info-box-template', {}).get(0)
+		var myOptions = {
+			content: this.boxText
+			,disableAutoPan: false
+			,maxWidth: 0
+			,pixelOffset: new google.maps.Size(-140, 0)
+			,zIndex: null
+			,closeBoxMargin: "10px 2px 2px 2px"
+			,closeBoxURL: "http://www.google.com/intl/en_us/mapfiles/close.gif"
+			,infoBoxClearance: new google.maps.Size(1, 1)
+			,isHidden: false
+			,pane: "floatPane"
+			,enableEventPropagation: false
+		};
+
+		// We create ourselves an infobox
+		this.infoBox = new InfoBox(myOptions);
+
+		this.render();
+	},
+
+	render: function(){
+	},
+
+	setIntersection: function(marker, model){
+		this.currentMarker = marker;
+		this.currentModel = model;
+
+		this.infoBox.close(); // close it if it's already on the map.
+		// set the content of the info box to that of the new intersection
+		this.boxText = $('<div></div>').loadTemplate('#info-box-template', model.attributes).get(0);
+		this.showMoreDom = $('#more-info-link').get(0);
+		this.infoBox.setContent(this.boxText);
+
+		// open it on the map
+		this.infoBox.open(this.map, marker);
+
+		// add an event listener to check for 'show more' click
+		google.maps.event.addDomListener(this.boxText, 'click', $.proxy(function(e){
+			// console.log(e);
+			this.infoBoxClicked(e);
+		}, this));
+	},
+
+	close: function(){
+		this.infoBox.close();
+	},
+
+	infoBoxClicked: function(e){
+		console.log(e);
+		console.log(e.toElement);
+		if($(this.boxText).find('#more-info-link').get(0) === e.toElement){
+			this.mapPageView.showMore(this.currentModel);
+		}
+	}
+
+});
