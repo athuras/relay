4cc85c8ccf0f32330aac3a591440a596628ba48b
Myles Tan <mylestan7@gmail.com>
2014-03-13 11:30:56 -0400

    panel is now hidden by default, and moved transitions to CSS

--- relay_flask/static/js/panel_view.js
+++ relay_flask/static/js/panel_view.js
@@ -18,33 +18,13 @@ var PanelView = Backbone.View.extend({
 	panelToggled: function(){
 		switch(this.$el.hasClass('expanded')){
 			case(true):
-				this.$('#panel-container').css('display', 'none');
-				this.$el.animate({
-					'min-width': '16px',
-					width: '16px'
-				}, 400);
-				this.$('#panel-toggle').css({
-					'transform' : 'rotate(180deg)',
-					'-ms-transform' : 'rotate(180deg)', /* IE 9 */
-					'-webkit-transform' : 'rotate(180deg)' /* Safari and Chrome */
-				})
+				this.$('#panel-container').removeClass('expanded');
 				this.$el.removeClass('expanded');
 				break;
 
 			case(false):
-				this.$el.animate({
-					'min-width': '238px',
-					width: '33%'
-				}, 400);
-				this.$('#panel-toggle').css({
-					'transform' : 'rotate(0deg)',
-					'-ms-transform' : 'rotate(0deg)', /* IE 9 */
-					'-webkit-transform' : 'rotate(0deg)' /* Safari and Chrome */
-				})
-				setTimeout(function(){
-					this.$('#panel-container').css('display', 'block');
-				}, 400);
 				this.$el.addClass('expanded');
+				this.$('#panel-container').addClass('expanded');
 				break;
 
 			default:
