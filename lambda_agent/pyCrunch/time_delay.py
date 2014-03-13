3bdbd22d26bd6c7156bedbf2128866d767a9eb33
Scott Neil <dscott.neil@gmail.com>
2014-03-12 20:41:47 -0400

    adding function for learning time delay. doesn't do anything yet

---
+++ lambda_agent/pyCrunch/time_delay.py
@@ -0,0 +1,15 @@
+def learn_time_delay(a, b):
+    '''
+    Given a set of data, need to learn the parameters of it's gamma
+
+    :a    -> entrance node on intersection
+    :b    -> exit node on neighbors intersection
+
+    TODO: Make this do stuff
+    '''
+
+    shape = 3
+    loc = 80
+    scale = 4
+
+    return (a, b, (shape, loc, scale))
