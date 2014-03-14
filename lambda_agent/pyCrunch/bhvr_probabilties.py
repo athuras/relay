34b6d41b052b1621b74e8c7a411eb23681771051
Scott Neil <dscott.neil@gmail.com>
2014-03-13 23:15:53 -0400

    1. Adding time delay functionality 2. refactored predictions 3. added tables to data for metrics 4. added some example data

---
+++ lambda_agent/pyCrunch/bhvr_probabilties.py
@@ -0,0 +1,8 @@
+
+def learn_bhvr_probabilities():
+    '''
+    Given data about past behaviour, flow, and historical edge probabilities,
+    calculate the new edge probabilities
+
+    TODO: Some stuff
+    '''
