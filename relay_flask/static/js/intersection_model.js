8df6e6ef9efcd984c0df0c82d9cf35c37a7b816d
Myles Tan <mylestan7@gmail.com>
2014-03-14 00:40:07 -0400

    added fake data

--- relay_flask/static/js/intersection_model.js
+++ relay_flask/static/js/intersection_model.js
@@ -1,23 +1,25 @@
 var IntersectionModel = Backbone.Model.extend({
 
-	defaults : { // So we know what should be in there.
-			'int_id': null,
+	defaults: { // So we know what should be in there.
+			// 'int_id': null,
 
-			'lat': null,
-			'long': null,
+			// 'lat': null,
+			// 'long': null,
 
-			'name': null,
-			'type': null,
-			'type_short': null,
+			// 'name': null,
+			// 'type': null,
+			// 'type_short': null,
 
-			//defaults for testing:
-			'status': 'OK',
-			'state': 'EWT',
-			'nextState': 'EWC',
-			'nextStateTime': '0:22'
+			// //defaults for testing:
+			// 'status': 'OK',
+			// 'state': 'EWT',
+			// 'nextState': 'EWC',
+			// 'nextStateTime': '0:22',
+
+			balloon: 'yes'
 	},
 
 	initialize: function() { // called automatically when created
-
+		
 	},
 });
