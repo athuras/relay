cdb39cc27184d03fa377723a0bcb2d61efe58627
Myles Tan <mylestan7@gmail.com>
2014-03-14 23:33:22 -0400

    cleaned up infobox, added click on arrow to open panel

--- relay_flask/static/js/info_box_view.js
+++ relay_flask/static/js/info_box_view.js
@@ -100,27 +100,6 @@ var InfoBoxView = Backbone.View.extend({
 			element: $(this.boxText).find('#legend').get(0)
 		});
 
-		// var graph = new Rickshaw.Graph( {
-		//         element: $(this.boxText).find('#graph').get(0),
-		//         width: 540,
-		//         height: 240,
-		//         series: [ {
-		//                 data: [ { x: -1893456000, y: 92228531 }, { x: -1577923200, y: 106021568 }, { x: -1262304000, y: 123202660 }, { x: -946771200, y: 132165129 }, { x: -631152000, y: 151325798 }, { x: -315619200, y: 179323175 }, { x: 0, y: 203211926 }, { x: 315532800, y: 226545805 }, { x: 631152000, y: 248709873 }, { x: 946684800, y: 281421906 }, { x: 1262304000, y: 308745538 } ], 
-		//                 color: 'steelblue'
-		//         } ]
-		// } );
-
-		// var x_axis = new Rickshaw.Graph.Axis.Time( { graph: graph } );
-
-		// var y_axis = new Rickshaw.Graph.Axis.Y( {
-		//         graph: graph,
-		//         orientation: 'left',
-		//         tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
-		//         element: $(this.boxText).find('#graph').get(0),
-		// } );
-
-		// graph.render();
-
 		// render graph
 		graph.render();
 
@@ -136,10 +115,11 @@ var InfoBoxView = Backbone.View.extend({
 		this.isOpen = false;
 	},
 
+	// listens for clicks and if they're on the link, we send the itersection to the panel.
 	infoBoxClicked: function(e){
 		console.log(e);
 		console.log(e.toElement);
-		if($(this.boxText).find('#more-info-link').get(0) === e.toElement){
+		if($(this.boxText).find('#chunk-arrow').get(0) === e.toElement){
 			this.mapPageView.showMore(this.currentModel);
 		}
 	}
