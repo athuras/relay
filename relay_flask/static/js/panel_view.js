ddb3666935f40f16094b87ba5c454329643ccfd6
Myles Tan <mylestan7@gmail.com>
2014-03-15 20:49:22 -0400

    some html for the panel and work on the panel action

--- relay_flask/static/js/panel_view.js
+++ relay_flask/static/js/panel_view.js
@@ -7,8 +7,9 @@ var PanelView = Backbone.View.extend({
 	},
 
 	initialize: function(){
-		this.chartOptions = bootstrap.chartOptions;
-		this.chartDataFormats = bootstrap.chartDataFormats;
+		// this.chartOptions = bootstrap.chartOptions;
+		// this.chartDataFormats = bootstrap.chartDataFormats;
+
 	},
 
 	render: function(){
@@ -46,33 +47,47 @@ var PanelView = Backbone.View.extend({
 	showIntersectionDetails: function(intersectionModel){
 		this.$('#intersection-title').text( intersectionModel.get('name') );
 
-		$.ajax({
-			type: 'POST',
-			datatype: 'JSON',
-			contentType: 'application/json',
-			url: 'http://localhost:5000/api/charts',
-			data: JSON.stringify({}),
-			async: false
-		}).then( function(d){
-			console.log(d);
-			var performance_data = bootstrap.chartDataFormats;
-			performance_data.labels = d.time;
-			performance_data.datasets[0].data = d.perf;
-			performance_data.datasets[1].data = d.vol;
+		// $.ajax({
+		// 	type: 'POST',
+		// 	datatype: 'JSON',
+		// 	contentType: 'application/json',
+		// 	url: 'http://localhost:5000/api/charts',
+		// 	data: JSON.stringify({}),
+		// 	async: false
+		// }).then( function(d){
+		// 	console.log(d);
+		// 	var performance_data = bootstrap.chartDataFormats;
+		// 	performance_data.labels = d.time;
+		// 	performance_data.datasets[0].data = d.perf;
+		// 	performance_data.datasets[1].data = d.vol;
 
-			var p_ctx = document.getElementById("performance-chart").getContext("2d");
-			var performanceChart = new Chart(p_ctx).Line(performance_data, this.chartOptions);
+		// 	var p_ctx = document.getElementById("performance-chart").getContext("2d");
+		// 	var performanceChart = new Chart(p_ctx).Line(performance_data, this.chartOptions);
 
-			var capacity_data = bootstrap.chartDataFormats;
-			capacity_data.labels = d.time;
-			capacity_data.datasets[0].data = d.costUD;
-			capacity_data.datasets[1].data = d.costLR;
+		// 	var capacity_data = bootstrap.chartDataFormats;
+		// 	capacity_data.labels = d.time;
+		// 	capacity_data.datasets[0].data = d.costUD;
+		// 	capacity_data.datasets[1].data = d.costLR;
 
-			var c_ctx = document.getElementById("capacity-chart").getContext("2d");
-			var capacityChart = new Chart(c_ctx).Line(capacity_data, this.chartOptions);
-		});
+		// 	var c_ctx = document.getElementById("capacity-chart").getContext("2d");
+		// 	var capacityChart = new Chart(c_ctx).Line(capacity_data, this.chartOptions);
+		// });
 
 		// this.$('#intersection-volume').text( intersectionModel.get('volume') );
 		// this.$('#intersection-performance').text( intersectionModel.get('performance') );
+
+		// For anything that requires real-time data, we use a set interval!
+		this.startInterval();
+	},
+
+	startInterval: function(){
+		this.interval = setInterval(function(pv){
+			// do your data pull
+			//update anyhting non-static.
+		}, this.updateFrequency, this)
+	},
+
+	stopInterval: function(){
+		clearInterval(this.interval);
 	}
 });
