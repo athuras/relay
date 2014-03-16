829351583b0ea3a58c58e061df63eccc1e7b5306
Myles Tan <mylestan7@gmail.com>
2014-03-16 11:34:32 -0400

    added live status panel

--- relay_flask/static/js/intersections_collection.js
+++ relay_flask/static/js/intersections_collection.js
@@ -24,19 +24,36 @@ var IntersectionsCollection = Backbone.Collection.extend({
 			async: false
         }).then( function(d){
         	data = d;
+        	s.restConnected();
+        	s.globalUpdate();
         });
 
-   //      $.ajax({
-			// type: "GET",
-			// datatype: "JSON",
-			// contentType: "application/json",
-			// url: "http://localhost:5000/intersections",
-			// async: false
-   //      }).then( function(d){
-   //      	data = d;
-   //      });
-
         this.add(data); // adds all the intersection objects to the collection
+	},
+
+	update: function(){
+		var bounds = new Object();
+		bounds.minlat = -90;
+		bounds.minlong = -180;
+		bounds.maxlat = 90;
+		bounds.maxlong = 180;
+
+		var data = null;
+
+		$.ajax({
+			type: "POST",
+			datatype: "JSON",
+			contentType: "application/json",
+			url: "http://localhost:5000/request_intersections",
+			data: JSON.stringify(bounds),
+			async: false
+        }).then( function(d){
+        	data = d;
+        	s.restConnected();
+        	s.globalUpdate();
+        });
+
+        this.set(data, {add: false}); // adds all the intersection objects to the collection
 	}
 
 });
