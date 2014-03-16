c2b1432862728134225c3054c7e9404d9840affe
Alexander Huras <alex.huras@gmail.com>
2014-03-16 15:52:57 -0400

    stable web app, build with make

--- lambda_agent/src/toppage_handler.erl
+++ lambda_agent/src/toppage_handler.erl
@@ -9,19 +9,27 @@ init(_Transport, Req, []) ->
 
 handle(Req, State) ->
     {Method, Req2} = cowboy_req:method(Req),
-    {Echo, Req3} = cowboy_req:qs_val(<<"HI THERE">>, Req2),
-    {ok, Req4} = echo(Method, Echo, Req3),
+    {Agent, Req3} = cowboy_req:qs_val(<<"agent">>, Req2),
+    Data = get_state(Agent),
+    {ok, Req4} = reply(Method, Data, Req3),
     {ok, Req4, State}.
 
-echo(<<"GET">>, undefined, Req) ->
-    cowboy_req:reply(400, [], <<"Missing Echo Parameter.">>, Req);
-echo(<<"GET">>, Echo, Req) ->
+reply(<<"GET">>, undefined, Req) ->
+    cowboy_req:reply(400, [], <<"missing agent parameter">>, Req);
+reply(<<"GET">>, Response, Req) ->
     cowboy_req:reply(200, [
             {<<"content-type">>, <<"test/plain; charset=utf-8">>}
-            ], Echo, Req);
-echo(_, _, Req) ->
+            ], Response, Req);
+reply(_, _, Req) ->
         %% Method not allowed
         cowboy_req:reply(405, Req).
 
 terminate(_Reason, _Req, _State) ->
     ok.
+
+get_state(Agent) ->
+    case lists:member(A=erlang:binary_to_atom(Agent, utf8),
+                      registered()) of
+        true -> gen_event:call(A, relay_store, request_state);
+        false -> undefined
+    end.
