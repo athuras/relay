8381e4d477dca06efb7b7036f67421cd598ca558
Scott Neil <dscott.neil@gmail.com>
2014-03-17 03:02:34 -0400

    queues now work...kinda

--- relay_flask/erlang_functions.py
+++ relay_flask/erlang_functions.py
@@ -10,7 +10,7 @@ def fetch_queues(int_id, length=150):
 
     # new queues, queue dict
 
-def fetch_status_info(int_id, old_queues, length=150):
+def fetch_status_info(int_id, length=150):
     r = rqs.get("http://localhost:8081/?agent=Agent1")
     data = r.json()
 
@@ -19,10 +19,10 @@ def fetch_status_info(int_id, old_queues, length=150):
     plan_codes = [b_parser.parse(b) for b in data['current_plan']]
     erl_info.append({'plans': plan_codes, 'plan_times': data['current_timing']})
 
-    new_queues = [data['ingress'],data['egress'],data['egress']]
-    updated_qs = merge_simulated_queues(new_queues, old_queues, length)
+    new_queues = [data['ingress'],data['egress']]
+    up_qs = mq.merge_simulated_queues(new_queues, length=150)
 
-    return erl_info, updated_qs, {'in': up_qs[0], 'out': up_qs[1], 'prediction': 0}
+    return erl_info, {'in': up_qs[0], 'out': up_qs[1], 'prediction': 0}
 
 ## UPDATING FUNCTIONS ##########################################################
 def update_plan(int_id):
